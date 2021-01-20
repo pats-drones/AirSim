74f025f7417138247c4b024c6f68916cafcfd515
Niek van Leeuwen <niekvleeuwen@hotmail.nl>
2021-01-20 11:33:05 +0100

    Changed setLedIntensity to simSetLedIntensity

--- AirLib/src/api/RpcLibClientBase.cpp
+++ AirLib/src/api/RpcLibClientBase.cpp
@@ -208,9 +208,9 @@ CollisionInfo RpcLibClientBase::simGetCollisionInfo(const std::string& vehicle_n
     return pimpl_->client.call("simGetCollisionInfo", vehicle_name).as<RpcLibAdapatorsBase::CollisionInfo>().to();
 }
 
-void RpcLibClientBase::setLedIntensity(float intensity, const std::string& vehicle_name)
+void RpcLibClientBase::simSetLedIntensity(float intensity, const std::string& vehicle_name)
 {
-    pimpl_->client.call("setLedIntensity", intensity, vehicle_name);
+    pimpl_->client.call("simSetLedIntensity", intensity, vehicle_name);
 }
 
 //sim only
